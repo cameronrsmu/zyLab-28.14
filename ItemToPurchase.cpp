3db374882be9b77130b1004b3e14000671d8006d
cameronrsmu <167338090+cameronrsmu@users.noreply.github.com>
2024-04-30 15:27:38 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,59 +1,53 @@
 #include "ItemToPurchase.h"
-#include <string>
 #include <iostream>
-using namespace std;
 
-//constructors
+// Constructor
 
-ItemToPurchase::ItemToPurchase(){
-  itemName = "";
-  itemDescription = "";
-  itemPrice = 0;
-  itemQuantity = 0;
-}
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity)
+    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
 
-ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity){
-  itemName = name;
-  itemDescription = description;
-  itemPrice = price;
-  itemQuantity = quantity;
+// Setters
+
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
 }
 
-//setters
-
-void ItemToPurchase::SetName(string name){
-  itemName = name;
-}
-void ItemToPurchase::SetDescription(string description){
-  itemDescription = description;
-} 
-void ItemToPurchase::SetPrice(int price){
-  itemPrice = price;
-}
-void ItemToPurchase::SetQuantity(int quantity){
-  itemQuantity = quantity;
+void ItemToPurchase::SetDescription(string description) {
+    itemDescription = description;
 }
 
-//getters
-
-string ItemToPurchase::GetName() const{
-  return itemName;
-}
-string ItemToPurchase::GetDescription() const{
-  return itemDescription;
-}
-int ItemToPurchase::GetPrice() const{
-  return itemPrice;
-}
-int ItemToPurchase::GetQuantity() const{
-  return itemQuantity;
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
 }
 
-//other functions
-
-void ItemToPurchase::PrintItemCost(){
-  cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << itemQuantity * itemPrice << endl;
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
 }
-void ItemToPurchase::PrintItemDescription(){
-  cout << itemName << ": " << itemDescription << endl;
+
+// Getters
+
+string ItemToPurchase::GetName() const {
+    return itemName;
+}
+
+string ItemToPurchase::GetDescription() const {
+    return itemDescription;
+}
+
+int ItemToPurchase::GetPrice() const {
+    return itemPrice;
+}
+
+int ItemToPurchase::GetQuantity() const {
+    return itemQuantity;
+}
+
+// Other Functions
+
+void ItemToPurchase::PrintItemCost() const {
+    cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << itemQuantity * itemPrice << endl;
+}
+
+void ItemToPurchase::PrintItemDescription() const {
+    cout << itemName << ": " << itemDescription << endl;
 }
